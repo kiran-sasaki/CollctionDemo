12274a92ccabed3401976d3af1c2ec4b28127410
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-03 11:44:01 +0530

    [Kiran] Add: Welcome Message

---
+++ CollectionsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace CollectionsDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to CollectionsDemo");
+        }
+    }
+}
