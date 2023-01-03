998ad5b73e481d19f188f5d1f2f0ac486e0f6093
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-03 11:52:34 +0530

    [Kiran] Add: List

--- CollectionsDemo/Program.cs
+++ CollectionsDemo/Program.cs
@@ -6,7 +6,7 @@ namespace CollectionsDemo
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to CollectionsDemo");
+            Colections.DoListDemo();
         }
     }
 }
